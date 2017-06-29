ce9e7f392ffafd6c66c6b543052280a36af80c23
Mohit Gupta <mohitg@adobe.com>
2017-06-29 07:37:53 +0530

    1. Fixed the Bounding Box Issue 2. Player handles key bard events now 3. Some refactoring in Player class.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
+//TODO: Variables like topmrgin which are sprite attributes, how should they be represented in the class
+//TODO: Collision Detection Needs to Happem
+//TODO: Clean update bounding box for Player
+//TODO: Restrict Player movement at boundaries
+//TODO: Winning condition
+//TODO: Add more artifacts & Points Scoring capabilities
 
+//*********************** ENEMY **************************************
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -43,28 +50,31 @@ Enemy.prototype.update = function(dt) {
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.beginPath();
     ctx.strokeStyle="red";
     ctx.rect(this.x, this.trueY, this.bugWidth, this.bugHeight);
     ctx.stroke();
     //console.log(this.x, this.y, this.bugWidth, this.bugHeight);
 };
 
+//***************** PLAYER *********************************************
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
     // initial location
-    this.generateLoc();
+    this.resetPosition();
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/char-cat-girl.png';
 };
 
 // Generates random position for player bot when it is born
-Player.prototype.generateLoc = function(){
+Player.prototype.resetPosition = function(){
     var rowheight = 83;
     var verticalCorrection = 33;
-    var topMargin = 60;
+    this.topMargin = 60;
     
     this.playerHeight = 78;
     this.playerWidth = 101;
@@ -73,21 +83,46 @@ Player.prototype.generateLoc = function(){
    
     this.x = 101 * randomcol;
     this.y = rowheight*6-verticalCorrection-this.playerHeight;
-    this.trueY= this.y + topMargin;
     console.log("Bug Position "+ this.x + ", "+this.y);
 };
 
-Player.prototype.update = function(dt) {
 
+Player.prototype.computeBounds = function(){
+    this.topLeftX = this.x;
+    this.topLeftY = this.y + this.topMargin;
+    this.bottomRightX= this.topLeftX + this.playerWidth;
+    this.bottomRightY = this.topLeftY + this.playerHeight; 
+}
+
+Player.prototype.update = function(dt) {
+    this.computeBounds ();
 };
 
 // Draw the player on the screen, required method for game
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    //ctx.strokeStyle="red";
-    //ctx.rect(this.x, this.trueY, this.playerWidth, this.playerHeight);
-    //ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle="yellow";
+    ctx.rect(this.topLeftX, this.topLeftY, this.playerWidth, this.playerHeight);
+    ctx.stroke();
     //console.log(this.x, this.y, this.playerWidth, this.playerHeight);
+};
+
+Player.prototype.handleInput = function(e) {
+    switch (e) {
+        case 'left':
+            this.x -= 20;
+            break;
+        case 'right':
+            this.x += 20;
+            break;
+        case 'up':
+            this.y -= 25;
+            break;
+        case 'down':
+            this.y += 25;
+            break;
+    }
 };
 
 // Now instantiate your objects.
@@ -113,5 +148,7 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
 
 
